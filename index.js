88704613a323eba917bd32f0a1796fbe955f5aed
Abhinav Gupta <abhinav6483@gmail.com>
2024-08-30 04:30:56 +0530

    1st commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,7 +23,38 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date",function(req,res){
+  const date_string = req.params.date;
+  let date;
+  if(!date_string.includes('-')){
+    date = new Date(Number.parseInt(date_string));
+  }
+  else{
+    date = new Date(date_string)
+  }
 
+    const unix_timestamp = Math.floor(date.getTime());
+    const utc_date = date.toUTCString();
+    if(utc_date=='Invalid Date'){
+      res.json({error: utc_date});
+    }
+    else
+    {
+      res.json({
+        unix: unix_timestamp,
+        utc: utc_date
+      })
+    }
+})
+app.get("/api",function(req,res){
+    let date = new Date()
+    const unix_timestamp = Math.floor(date.getTime());
+    const utc_date = date.toUTCString();
+      res.json({
+        unix: unix_timestamp,
+        utc: utc_date
+      })
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
